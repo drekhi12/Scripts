c0030e48f3562fb756155df7757838ec8fc24e89
vaibhavdaren <vaibhavdaren@gmail.com>
2017-10-21 04:20:21 +0530

    Update shutdown.py

--- Python2 Linux/shutdown.py
+++ Python2 Linux/shutdown.py
@@ -1,9 +1,13 @@
-import dbus
-sys_bus = dbus.SystemBus()
-hal_srvc = sys_bus.get_object('org.freedesktop.Hal',
-                              '/org/freedesktop/Hal/devices/computer')
-pwr_mgmt =  dbus.Interface(hal_srvc,
-                'org.freedesktop.Hal.Device.SystemPowerManagement')
-shutdown_method = pwr_mgmt.get_dbus_method("Shutdown")
-shutdown_method()
-
+import os
+while True:
+	print("1. Shutdown Computer")
+	print("2. Restart Computer")
+	print("3. Exit")
+	choice = int(input("Enter your choice: "))
+	if(choice>=1 and choice<=2):
+		if choice == 1:
+			os.system("shutdown /s /t 1")
+		else:
+			os.system("shutdown /r /t 1")
+	else:
+		break
